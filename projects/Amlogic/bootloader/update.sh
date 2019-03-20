48de161286b3b30a3752a4344599abff2c89e97a
Team CoreELEC <team@coreelec.org>
2019-03-20 04:05:30 +0000

    projects/Amlogic: add subdevice support for Odroid_N2

--- projects/Amlogic/bootloader/update.sh
+++ projects/Amlogic/bootloader/update.sh
@@ -45,6 +45,9 @@ for arg in $(cat /proc/cmdline); do
 	    SUBDEVICE="Odroid_C2"
 	    LE_DT_ID="gxbb_p200_2g_odroid_c2"
             ;;
+          *odroid_n2)
+	    SUBDEVICE="Odroid_N2"
+            ;;
         esac
       fi
 
