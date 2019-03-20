69cb3927d576d9b5740c0bc7bf30a7f5375ad847
Team CoreELEC <team@coreelec.org>
2019-03-20 04:04:47 +0000

    u-boot-Odroid_N2: introduce new package

---
+++ projects/Amlogic/packages/u-boot-Odroid_N2/package.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="u-boot-Odroid_N2"
+PKG_VERSION="655eafccb7cf90aa75c74010e07065efac605a4e"
+PKG_SHA256="6ee727e6657e475d94c7be644a2f93479de47526eb608ae06d1b6f83691e24aa"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://www.denx.de/wiki/U-Boot"
+PKG_URL="https://github.com/CoreELEC/u-boot/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain gcc-linaro-aarch64-elf:host gcc-linaro-arm-eabi:host u-boot_firmware"
+PKG_LONGDESC="Das U-Boot is a cross-platform bootloader for embedded systems."
+PKG_TOOLCHAIN="manual"
+
+configure_package() {
+  PKG_UBOOT_CONFIG="odroidn2_defconfig"
+}
+
+make_target() {
+  [ "${BUILD_WITH_DEBUG}" = "yes" ] && PKG_DEBUG=1 || PKG_DEBUG=0
+  export PATH=$TOOLCHAIN/lib/gcc-linaro-aarch64-elf/bin/:$TOOLCHAIN/lib/gcc-linaro-arm-eabi/bin/:$PATH
+  DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make mrproper
+  DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make $PKG_UBOOT_CONFIG
+  DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make HOSTCC="$HOST_CC" HOSTSTRIP="true"
+}
+
+makeinstall_target() {
+    : # nothing
+}
